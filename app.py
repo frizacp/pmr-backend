ab4f719f8844233779563f9ce2b7be6347e1b832
Friza Cahya <frizaproject@gmail.com>
2024-02-20 00:54:51 +0700

    Revisi getdata_all v1

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import date
 import mysql.connector
 import json
 from datetime import datetime
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -17,6 +18,13 @@ db_config = {
     'host': 'localhost',
     'user': 'n1569631_admin',
     'password': 'Ohno210500!',
+    'database': 'n1569631_pickmyrace'
+}
+
+db_config_2 = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
     'database': 'n1569631_pickmyrace'
 }
 
@@ -142,18 +150,40 @@ def get_alldata():
         cursor = connection.cursor(dictionary=True)
 
         query = '''
-            SELECT data_pelari.bib, data_pelari.name,data_pelari.gender,data_pelari.contest,
+            SELECT data_pelari.bib, data_pelari.firstName,data_pelari.gender,data_pelari.contest,data_pelari.category,
             finish.finishtime,finish.chiptime,finish.overallplace,finish.divisionplace, finish.pace,
             cp1.finishtime AS cp1,
             cp2.finishtime AS cp2,
             cp3.finishtime AS cp3,
-            cp4.finishtime AS cp4
+            cp4.finishtime AS cp4,
+            cp5.finishtime AS cp5,
+            cp6.finishtime AS cp6,
+            cp7.finishtime AS cp7,
+            cp8.finishtime AS cp8,
+            cp9.finishtime AS cp9,
+            cp10.finishtime AS cp10,
+            cp11.finishtime AS cp11,
+            cp12.finishtime AS cp12,
+            cp13.finishtime AS cp13,
+            cp14.finishtime AS cp14,
+            cp15.finishtime AS cp15
             FROM data_pelari 
             LEFT JOIN finish ON data_pelari.bib=finish.bib 
             LEFT JOIN cp1 ON data_pelari.bib=cp1.bib 
             LEFT JOIN cp2 ON data_pelari.bib=cp2.bib 
             LEFT JOIN cp3 ON data_pelari.bib=cp3.bib 
-            LEFT JOIN cp4 ON data_pelari.bib=cp4.bib;
+            LEFT JOIN cp4 ON data_pelari.bib=cp4.bib
+            LEFT JOIN cp5 ON data_pelari.bib=cp5.bib
+            LEFT JOIN cp6 ON data_pelari.bib=cp6.bib
+            LEFT JOIN cp7 ON data_pelari.bib=cp7.bib
+            LEFT JOIN cp8 ON data_pelari.bib=cp8.bib
+            LEFT JOIN cp9 ON data_pelari.bib=cp9.bib
+            LEFT JOIN cp10 ON data_pelari.bib=cp10.bib
+            LEFT JOIN cp11 ON data_pelari.bib=cp11.bib
+            LEFT JOIN cp12 ON data_pelari.bib=cp12.bib
+            LEFT JOIN cp13 ON data_pelari.bib=cp13.bib
+            LEFT JOIN cp14 ON data_pelari.bib=cp14.bib
+            LEFT JOIN cp15 ON data_pelari.bib=cp15.bib
         '''
 
         cursor.execute(query)
@@ -164,15 +194,19 @@ def get_alldata():
         # Menutup kursor dan koneksi
         cursor.close()
         connection.close()
-        now = datetime.now()
-        dt = now.strftime("%H:%M:%S")
+
+        df = pd.DataFrame.from_dict(results)
+        df['count'] = df[['cp1', 'cp2', 'cp3', 'cp4', 'cp5', 'cp6', 'cp7', 'cp8', 'cp9', 'cp10','cp11','cp12','cp13','cp14','cp15',]].count(axis=1)
+        results_pandas = df.to_dict(orient='records')
+
         # Inisialisasi dictionary untuk menyimpan nilai unik kolom "contest"
         contest_values = [row['contest'] for row in results]
 
         # Mengonversi list menjadi set untuk mendapatkan nilai unik
         unique_contests = set(contest_values)
-
-        return jsonify({'status': 'success', 'data': results,'date':dt ,'list_contest':list(unique_contests)})
+        now = datetime.now()
+        dt = now.strftime("%H:%M:%S")
+        return jsonify({'status': 'success', 'data': results_pandas,'date':dt ,'list_contest':list(unique_contests)})
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
 
