9cd24e8c06adcdd85e8b0b5fdd84ad5fca3f07c8
Friza Cahya <frizaproject@gmail.com>
2024-02-24 01:21:36 +0700

    Fix bug getdata_all count dibawah aja

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-db_config = {
+db_config_9 = {
     'host': 'localhost',
     'user': 'n1569631_admin',
     'password': 'Ohno210500!',
@@ -27,7 +27,7 @@ db_config_2 = {
     'database': 'n1569631_pickmyrace'
 }
 
-db_config_9 = {
+db_config = {
     'host': '156.67.213.247',
     'user': 'n1569631_admintagcheck',
     'password': 'Ohno210500!',
@@ -196,8 +196,9 @@ def get_alldata():
         connection.close()
 
         df = pd.DataFrame.from_dict(results)
-        df = df.fillna('')
+        
         df['count'] = df[['ws1', 'ws2', 'ws3', 'ws4', 'ws5', 'ws6', 'ws7', 'ws8', 'ws9', 'ws10','ws11','ws12']].count(axis=1)
+        df = df.fillna('')
         results_pandas = df.to_dict(orient='records')
 
         # Inisialisasi dictionary untuk menyimpan nilai unik kolom "contest"
